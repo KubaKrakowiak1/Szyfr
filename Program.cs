d34b5cbaa87d0967df70352af723f81bfd830333
KubaKrakowiak1 <167287455+KubaKrakowiak1@users.noreply.github.com>
2025-12-16 14:21:22 +0100

    aplikacja konsolowa

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        szyfr();
+    }
+
+    static void szyfr()
+    {
+        string haslo = "abc";
+        int key = 3;
+        char[] alfabet = "abcdefghijklmnopqrstuwxyz".ToCharArray();
+
+        char[] wynik = new char[haslo.Length];
+
+        for (int i = 0; i < haslo.Length; i++)
+        {
+            
+            int index = haslo[i] - 'a';
+            int newIndex = (index + key) % 26;
+            wynik[i] = alfabet[newIndex];
+        }
+        string zaszyfrowaneHaslo = new string(wynik);
+
+        Console.WriteLine(zaszyfrowaneHaslo);
+    }
+}
